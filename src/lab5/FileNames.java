ac8b351484b37e025ca2575fe417fbc5c746f7dc
habibaelghazouly <habibahoussam7@gmail.com>
2024-11-02 10:00:33 +0200

    first 3

---
+++ src/lab5/FileNames.java
@@ -0,0 +1,9 @@
+
+package lab5;
+
+public interface FileNames { 
+String TRAINER_FILENAME = "Trainers.txt"; 
+String MEMBER_FILENAME = "Members.txt"; 
+String CLASS_FILENAME = "Class.txt"; 
+String REGISTRATION_FILENAME = "Registration.txt"; 
+} 
---
+++ src/lab5/LoginCredentials.java
@@ -0,0 +1,10 @@
+
+package lab5;
+
+
+public interface LoginCredentials { 
+String ADMIN_USERNAME = "admin"; 
+String ADMIN_PASSWORD = "12345"; 
+String TRAINER_USERNAME = "trainer"; 
+String TRAINER_PASSWORD = "56789"; 
+} 
